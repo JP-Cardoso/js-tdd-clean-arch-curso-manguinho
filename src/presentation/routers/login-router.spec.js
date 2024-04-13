60c652509f7deee245364b472aca69e2ff4d8187
JP-Cardoso <jhonpoal@hotmail.com>
2024-04-13 00:43:08 -0300

    test: ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -101,4 +101,36 @@ describe('Login router', () => {
     expect(httpResponse.statusCode).toBe(401);
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+
+  test('should return 500 if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter();
+
+    httpRequest = {
+      body: {
+        email: 'any_email@.com.br',
+        password: 'any_151851'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+  })
+
+  test('should return 500 if no AuthUseCase has no auth method', () => {
+
+    class AuthUseCaseSpy { };
+    const authUseCase = new AuthUseCaseSpy();
+    const sut = new LoginRouter(authUseCase);
+
+    httpRequest = {
+      body: {
+        email: 'any_email@.com.br',
+        password: 'any_151851'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+  })
 })
