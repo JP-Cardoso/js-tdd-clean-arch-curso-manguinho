6db49d9410eff7461ecf256b408c1265e9a63029
JP-Cardoso <jhonpoal@hotmail.com>
2024-04-13 00:17:35 -0300

    feat: ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,17 +1,32 @@
 /**
- * sut -> system under test
+ * sut -> system under test;
+ * 
+ * spy -> Mock que captura valores e faz comparações
+ * 
  */
 
 const LoginRouter = require('./login-router');
 const MissingParamerror = require('../helpers/missing-param-error');
 
+//factory pattern
+//esse cara é um fake
 const makeSut = () => {
-  return new LoginRouter();
+
+  class AuthUseCaseSpy {
+    auth(email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+  const authUseCaseSpy = new AuthUseCaseSpy();
+  const sut = new LoginRouter(authUseCaseSpy);
+
+  return { authUseCaseSpy, sut };
 }
 
 describe('Login router', () => {
   test('should return 400 if no email is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
 
     httpRequest = {
       body: {
@@ -26,7 +41,7 @@ describe('Login router', () => {
   })
 
   test('should return 400 if no password is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
 
     httpRequest = {
       body: {
@@ -41,23 +56,32 @@ describe('Login router', () => {
   })
 
   test('should return 500 if no httpRequest is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpResponse = sut.route();
     expect(httpResponse.statusCode).toBe(500);
   })
 
   test('should return 500 if no httpRequest has no body', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {};
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
   })
 
   test('should call AuthUseCase with correct params', () => {
-    const sut = makeSut();
-    const httpRequest = {};
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
+    const { sut, authUseCaseSpy } = makeSut();
+
+    httpRequest = {
+      body: {
+        email: 'any_email@.com.br',
+        password: '151851'
+      }
+    }
+
+    sut.route(httpRequest);
+
+    expect(authUseCaseSpy.email).toEqual(httpRequest.body.email);
+    expect(authUseCaseSpy.password).toEqual(httpRequest.body.password);
   })
 
 })
