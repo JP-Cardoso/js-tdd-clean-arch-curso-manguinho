2bb97609742f6e381fed4659b40e52430e0c6474
JP-Cardoso <jhonpoal@hotmail.com>
2024-04-14 21:52:54 -0300

    test: ensure EmailValidator return false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,6 +1,8 @@
+const validator = require('validator');
+
 class EmailValidator {
   isValid(email) {
-    return true
+    return validator.isEmail(email);
   }
 }
 
@@ -9,5 +11,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator();
     const isEmailValid = sut.isValid('valid_email@email.com');
     expect(isEmailValid).toBeTruthy();
+  })
+
+  test('should return true if validator return false', () => {
+    validator.isEmailValid = false;
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid('');
+    expect(isEmailValid).toBeFalsy();
   })
 })
