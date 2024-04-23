5247a5208e861920f46ea659511021c2c6bda93e
JP-Cardoso <jhonpoal@hotmail.com>
2024-04-22 23:48:57 -0300

    make sut creation to a factory helper method

--- src/domain/use-cases/auth-use-case.spec.js
+++ src/domain/use-cases/auth-use-case.spec.js
@@ -25,36 +25,45 @@ const makeAuthUseCase = (repository) => {
   return new AuthUseCase(repository);
 }
 
+const makeSut = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load(email) {
+      this.email = email;
+    }
+  }
+
+  const loadUserByEmailRepository = new LoadUserByEmailRepositorySpy();
+  const sut = makeAuthUseCase(loadUserByEmailRepository);
+
+  return {
+    loadUserByEmailRepository,
+    sut
+  }
+}
+
 
 describe('Auth UseCase', () => {
   test('should throw if no email is provided', async () => {
-    const sut = makeAuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamerror('email'));
   })
 
   test('should throw if no password is provided', async () => {
     const email = "any_email@email.com";
-    const sut = makeAuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth(email);
     expect(promise).rejects.toThrow(new MissingParamerror('password'));
   })
 
   test('should call loadUserByEmailRepository with correct email', async () => {
-
     const credentials = {
       email: "any_email@email.com",
       password: "any_password"
     }
 
-    class LoadUserByEmailRepository {
-      async load(email) {
-        this.email = email;
-      }
-    }
+    const { sut, loadUserByEmailRepository } = makeSut();
 
-    const loadUserByEmailRepository = new LoadUserByEmailRepository();
-    const sut = makeAuthUseCase(loadUserByEmailRepository);
     sut.auth(credentials.email, credentials.password);
     expect(loadUserByEmailRepository.email).toBe(credentials.email);
   })
